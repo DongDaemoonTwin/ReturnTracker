5c6e562227cc9d0ac72815dfe6245099668a7b24
DongDaemoonTwin <godhand3024@gmail.com>
2026-08-11 00:38:11 +0900

    Fix Compose scoped weight usage

--- android/app/src/main/java/com/returntracker/android/ui/AddItemSheet.kt
+++ android/app/src/main/java/com/returntracker/android/ui/AddItemSheet.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
